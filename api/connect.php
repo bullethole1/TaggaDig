b4a899138206abb8e00fe90fa6854a5b7c600381
Lina <linagreen_@hotmail.com>
2017-02-02 13:36:14 +0100

    login form

---
+++ api/connect.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<meta charset="utf-8">
+    </head>
+    <body>
+<form action="login.php" method="POST">
+     <input type="text" name="username" value="" placeholder="Användarnamn" /><br>
+     <input type="password" name="password" value="" placeholder="Lösenord"/><br> 
+    <button type="submit">Submit</button>
+</form>
+
+
+
+</body>
+</html>
+
+
