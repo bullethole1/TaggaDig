149e7663f191044f0a76a83451425ca7e0890480
Lina <linagreen_@hotmail.com>
2017-02-03 10:15:43 +0100

    created session functions

---
+++ api/session.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+// om fel inlogg, skriv ut errormedd. 
+function error_message(){
+
+if(isset($_SESSION['message'])  ){
+ $output = ($_SESSION['message']); 
+$_SESSION['message'] = null;
+return $output;
+}
+
+}
+
+
+// OM man inte är inloggad skicka tillbaka till connect.php
+function  failed(){
+    if(!isset ($_SESSION['logged_in']) ){
+ header("Location: connect.php");
+          exit;
+}
+   
+}
+
+
+?>
