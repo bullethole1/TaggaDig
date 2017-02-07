aac88fb6341538e1f53bf2076c4a31ed308ebf3e
Lina <linagreen_@hotmail.com>
2017-02-07 10:28:13 +0100

    added model in fieldlist

--- api/boka.php
+++ api/boka.php
@@ -7,12 +7,5 @@ include_once('database.php');
 require_once("session.php"); 
 require_once("create.php");
 
-// $id = addslashes( $_REQUEST['id']);
 
-// $image = mysql_query("SELECT * FROM `orders` WHERE id = $id");
-// $image = mysql_fetch_assoc($image);
-// $image = $image['upFile'];
-
-// header ("Content-type: image/jpeg");
-// echo $image;
 ?>
--- api/login.php
+++ api/login.php
@@ -1,3 +1,5 @@
+ <?php include_once('database.php');
+ ?>
  <!DOCTYPE html>
 <html>
     <head>
@@ -19,18 +21,18 @@ $_SESSION['logged_in'] = $_POST['email'];
 
 failed();      
 
-$host = '127.0.0.1'; 
-$db = 'taggadig';
-$user = 'root';
-$password = 'root';
-$charset = 'utf8';
+// $host = '127.0.0.1'; 
+// $db = 'taggadig';
+// $user = 'root';
+// $password = 'root';
+// $charset = 'utf8';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-			 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-			 PDO::ATTR_EMULATE_PREPARES   => false  ];
+// $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+// $options = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+// 			 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+// 			 PDO::ATTR_EMULATE_PREPARES   => false  ];
 
-$pdo = new PDO ($dsn, $user, $password, $options);
+// $pdo = new PDO ($dsn, $user, $password, $options);
 
 
 
