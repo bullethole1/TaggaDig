503ecf26d7d3b5e54285db6f828707f7d0775fcd
Lina <linagreen_@hotmail.com>
2017-02-08 11:16:32 +0100

    admin.php

--- api/admin.php
+++ api/admin.php
@@ -2,52 +2,33 @@
 
 include_once 'database.php';
 require_once "session.php";
+?>
+<h2> Användare </h2>    
 
-echo "<h2> Användare </h2>";
-$sql = "SELECT *  FROM `members` ";
-$stm_member = $pdo->prepare($sql);
-$stm_member->execute([]);
-foreach ($stm_member as $row) {
-   
-    ?> 
-    <form  action ="admin.php" method="POST">
-     <button type="submit" name="delete" value="<?php  echo $row['id'] ?>"   >TA BORT</button>
-     </form>
-<?php 
-    echo $row['id'] . " ."; 
-    echo $row['username'] . " ";
-    echo $row['business'] . " ";
-    echo $row['firstName'] . " ";
-    echo $row['lastName'] . " ";
-    echo $row['email'] . " ";
-    echo "<br>";
-    echo "<br>";
-}
+<?php
+// hämta alla användare
+$sql = "SELECT  `id`, `business`, `firstName`, `lastName`, `email` FROM members WHERE `id` > 0";
+$row=$pdo->prepare($sql);
+$row->execute();
+$result=$row->fetchAll(PDO::FETCH_ASSOC);
+$main = array('data'=>$result);
 
-echo "<h2> Ordrar </h2>";
-$sql = "SELECT *  FROM `orders` ";
-$stm_order = $pdo->prepare($sql);
-$stm_order->execute([]);
-foreach ($stm_order as $row) {
-    
-    ?> 
-    <form  action ="admin.php" method="POST">
-     <button type="submit" name="delete_order" value="<?php echo $row['orderNr'] ?>">TA BORT</button>
-     </form>
-<?php 
-    echo $row['orderNr'] . " .";
-    echo $row['area'] . " ";
-    echo $row['model'] . " ";
-    echo $row['firstName'] . " ";
-    echo $row['lastName'] . " ";
-    echo $row['email'] . " ";
-    echo $row['phoneNumber'] . " ";
-    echo $row['businessName'] . " ";
-    echo $row['description'] . " ";
+echo json_encode($main); 
+?> 
+  
+<h2> Ordrar </h2>
+<?php
+// hämta alla ordrar
 
-    echo "<br>";
-    echo "<br>";
-}
+$sql_order = "SELECT *  FROM `orders` WHERE `orderNr` > 0 ";
+$row = $pdo->prepare($sql_order);
+$row->execute();
+$resultat=$row->fetchAll(PDO::FETCH_ASSOC);
+$main_order = array('data'=> $resultat);
+echo json_encode($main_order);
+
+
+
 
 
 if(isset($_POST['delete_order'])){
