c6455f7e4cc65d7d35f9f733e10b95a814b09a1f
EmilKarlsson91 <emil.m.karlsson91@gmail.com>
2017-02-09 13:16:39 +0100

    Added function for map

--- script/map-script.js
+++ script/map-script.js
@@ -24,60 +24,267 @@ $(document).ready(function() {
         var vasastan = {lat:57.6951428, lng: 11.9682636};
         var gronsakstorget =  {lat: 57.70223859999999, lng: 11.965698599999996};
 
-        var map = new google.maps.Map(document.getElementById('map'), {
+         map = new google.maps.Map(document.getElementById('map'), {
           zoom: 14,
           center: centralen
         });
 
-         var contentString = '<div id="content">'+
+     
+         var contentStringKorsvegen = '<div id="content">'+
             '<div id="siteNotice">'+
             '</div>'+
             '<h1 id="firstHeading" class="firstHeading">Korsvägen</h1>'+
-            '<div id="bodyContent">'+
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-            'sandstone rock formation in the southern part of the '+
-            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-            'south west of the nearest large town, Alice Springs; 450&#160;km '+
-            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-            'Aboriginal people of the area. It has many springs, waterholes, '+
-            'rock caves and ancient paintings. Uluru is listed as a World '+
-            'Heritage Site.</p>'+
-           	'<button type="button">Boka</button>'
+            '<label>Område:</label> Korsvägen'+
+            '<br>'+
+            '<label>Typ:</label> Mobile/Display' +
+            '<br>' +
+            '<label>Storlek:</label>' +
+            '<br>'+ 
+            '<label>Räckvidd:</label>' +
+            '<br>'+
+            '<label>Pris:</label>' +
+            '<br>' +
             '</div>'+
             '</div>';
 
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
+             var contentStringBrunnsparken = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Brunnsparken</h1>'+
+            '<label>Område:</label> Brunnsparken'+
+            '<br>'+
+            '<label>Typ:</label> Mobile/Display' +
+            '<br>' +
+            '<label>Storlek:</label>' +
+            '<br>'+ 
+            '<label>Räckvidd:</label>' +
+            '<br>'+
+            '<label>Pris:</label>' +
+            '<br>' +
+            '</div>'+
+            '</div>';
+
+            var contentStringVasastan = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Vasastan</h1>'+
+            '<label>Område:</label> Vasastan'+
+            '<br>'+
+            '<label>Typ:</label> Mobile/Display' +
+            '<br>' +
+            '<label>Storlek:</label>' +
+            '<br>'+ 
+            '<label>Räckvidd:</label>' +
+            '<br>'+
+            '<label>Pris:</label>' +
+            '<br>' +
+            '</div>'+
+            '</div>';
+
+             var contentStringGotaplatsen = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Gotaplatsen</h1>'+
+            '<label>Område:</label> Götaplatsen'+
+            '<br>'+
+            '<label>Typ:</label> Mobile/Display' +
+            '<br>' +
+            '<label>Storlek:</label>' +
+            '<br>'+ 
+            '<label>Räckvidd:</label>' +
+            '<br>'+
+            '<label>Pris:</label>' +
+            '<br>' +
+            '</div>'+
+            '</div>';
+
+            var contentStringOstra = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Östra Hamngatan</h1>'+
+            '<label>Område:</label> Östra Hamngatan'+
+            '<br>'+
+            '<label>Typ:</label> Mobile/Display' +
+            '<br>' +
+            '<label>Storlek:</label>' +
+            '<br>'+ 
+            '<label>Räckvidd:</label>' +
+            '<br>'+
+            '<label>Pris:</label>' +
+            '<br>' +
+            '</div>'+
+            '</div>';
+
+
+            var contentStringSvingeln = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Svingeln</h1>'+
+            '<label>Område:</label> Svingeln'+
+            '<br>'+
+            '<label>Typ:</label> Mobile/Display' +
+            '<br>' +
+            '<label>Storlek:</label>' +
+            '<br>'+ 
+            '<label>Räckvidd:</label>' +
+            '<br>'+
+            '<label>Pris:</label>' +
+            '<br>' +
+            '</div>'+
+            '</div>';
+
+            var contentStringGronsak = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Grönskastorget</h1>'+
+            '<label>Område:</label> Grönskastorget'+
+            '<br>'+
+            '<label>Typ:</label> Mobile/Display' +
+            '<br>' +
+            '<label>Storlek:</label>' +
+            '<br>'+ 
+            '<label>Räckvidd:</label>' +
+            '<br>'+
+            '<label>Pris:</label>' +
+            '<br>' +
+            '</div>'+
+            '</div>';
+
+            var contentStringKung = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Kungsportsplatsen</h1>'+
+            '<label>Område:</label> Kungsportsplatsen'+
+            '<br>'+
+            '<label>Typ:</label> Mobile/Display' +
+            '<br>' +
+            '<label>Storlek:</label>' +
+            '<br>'+ 
+            '<label>Räckvidd:</label>' +
+            '<br>'+
+            '<label>Pris:</label>' +
+            '<br>' +
+            '</div>'+
+            '</div>';
+
+
+        var infowindow1 = new google.maps.InfoWindow({
+          content: contentStringKorsvegen,
         });
+
+
+        var infowindow2 = new google.maps.InfoWindow({
+          content: contentStringBrunnsparken,
+        });
+
+
+        var infowindow3 = new google.maps.InfoWindow({
+          content: contentStringOstra,
+        });
+
+
+        var infowindow4 = new google.maps.InfoWindow({
+          content: contentStringGotaplatsen
+        });
+
+         var infowindow5 = new google.maps.InfoWindow({
+          content: contentStringSvingeln
+        });
+
+         var infowindow6 = new google.maps.InfoWindow({
+          content: contentStringKung
+        });
+
+        var infowindow7 = new google.maps.InfoWindow({
+          content: contentStringVasastan
+        });
+
+        var infowindow8 = new google.maps.InfoWindow({
+          content: contentStringGronsak
+        });
+
+        var infowindow9 = new google.maps.InfoWindow({
+          content: contentStringGotaplatsen
+        });
+
+
+        var windowList = [infowindow1,infowindow2,infowindow3,infowindow4,infowindow5,infowindow6,infowindow7,infowindow8];
 
         var marker1 = new google.maps.Marker({
           position: korsvagen,
-          map: map,
+          map: map
         });
+
         marker1.addListener('click', function() {
-          infowindow.open(map, marker1);
+          for(i=0; i <= windowList.length; i++){
+            if(windowList[i] === infowindow1){
+              infowindow1.open(map, marker1);  
+            }
+            else {
+                windowList[i].close();
+            }
+          }  
         });
 
         var marker2 = new google.maps.Marker({
           position: brunnsparken,
           map: map
         });
+        marker2.addListener('click', function() {
+           for(i=0; i <= windowList.length; i++){
+            if(windowList[i] === infowindow2){
+              infowindow2.open(map, marker2);  
+            }
+            else {
+                windowList[i].close();
+            }
+          }
+        });
+
 
          var marker3 = new google.maps.Marker({
           position: ostra_hamngatan,
           map: map
+        });
+        marker3.addListener('click', function() {
+           for(i=0; i <= windowList.length; i++){
+            if(windowList[i] === infowindow3){
+              infowindow3.open(map, marker3);  
+            }
+            else {
+                windowList[i].close();
+            }
+          }
         });
 
         var marker4 = new google.maps.Marker({
           position: gotaplatsen,
           map: map
         });
+        marker4.addListener('click', function() {
+           for(i=0; i <= windowList.length; i++){
+            if(windowList[i] === infowindow4){
+              infowindow4.open(map, marker4);  
+            }
+            else {
+                windowList[i].close();
+            }
+          }
+        });
 
          var marker5 = new google.maps.Marker({
           position: svingeln,
           map: map
+        });
+        marker5.addListener('click', function() {
+           for(i=0; i <= windowList.length; i++){
+            if(windowList[i] === infowindow5){
+              infowindow5.open(map, marker5);  
+            }
+            else {
+                windowList[i].close();
+            }
+          }
         });
 
 
@@ -85,10 +292,45 @@ $(document).ready(function() {
           position: kungsportsplatsen,
           map: map
         });
+        marker6.addListener('click', function() {
+           for(i=0; i <= windowList.length; i++){
+            if(windowList[i] === infowindow6){
+              infowindow6.open(map, marker6);  
+            }
+            else {
+                windowList[i].close();
+            }
+          }
+        });
 
-         var marker7 = new google.maps.Marker({
+        var marker7 = new google.maps.Marker({
           position: vasastan,
           map: map
+        });
+        marker7.addListener('click', function() {
+           for(i=0; i <= windowList.length; i++){
+            if(windowList[i] === infowindow7){
+              infowindow7.open(map, marker7);  
+            }
+            else {
+                windowList[i].close();
+            }
+          }
+        });
+
+        var marker8 = new google.maps.Marker({
+          position: gronsakstorget,
+          map: map
+        });
+        marker8.addListener('click', function() {
+           for(i=0; i <= windowList.length; i++){
+            if(windowList[i] === infowindow8){
+              infowindow8.open(map, marker8);  
+            }
+            else {
+                windowList[i].close();
+            }
+          }
         });
 
       }
