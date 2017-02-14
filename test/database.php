f084be82c655360194d5a7c2eda7e2b71cf86831
Lina <linagreen_@hotmail.com>
2017-02-14 10:13:39 +0100

    databasinställningar

--- test/database.php
+++ test/database.php
@@ -1,19 +1,14 @@
 <?php
 
-    	$dbHost = 'localhost';
-		$db = 'taggadig';
-		$dbUser = 'root';
-		$password = 'root';
-		$charset = 'utf8';
-		$dsn = "mysql:host=$dbHost;dbname=$db;charset=$charset";
-		$options = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-					PDO::ATTR_EMULATE_PREPARES   => false  ];
-
-	try {
-    	$pdo = new PDO($dsn, $dbUser, $password, $options);
-	} catch (PDOException $e) {
+$dbHost = 'mysql525.loopia.se';
+$db = 'zocomutbildning_se_db_7';
+$dbUser = 'taggadig@z164631';
+$password = 'ta55ad1g234';
+$charset = 'utf8';
+$dsn = "mysql:host={$dbHost};dbname={$db};charset={$charset}";
+$options = [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC, \PDO::ATTR_EMULATE_PREPARES => false];
+try {
+    $pdo = new \PDO($dsn, $dbUser, $password, $options);
+} catch (\PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
-
-?>
