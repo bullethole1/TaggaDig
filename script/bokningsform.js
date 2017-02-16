32bcb9b30b5dc1598f4c9e1e0c06766591dde2d0
Goran <GoranKadir@gmail.com>
2017-02-16 11:38:44 +0100

    inget viktigt

--- script/bokningsform.js
+++ script/bokningsform.js
@@ -1,10 +1,14 @@
  $(document).ready(function() {
 
 
- 
- $("#tryckboka").click(function() {
- 	
+ 	$.getJSON("http://taggadig.zocomutbildning.se/test/show_members_admin.php", function(data){
+ 		var newdata = data.data;
+
+ 		 for( var i of newdata) {
+              i.area;
+             }
+
+ 	});
 
  });
 
-});
