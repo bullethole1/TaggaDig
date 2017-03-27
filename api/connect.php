bd9a13ff792575be2af584f60905f1397bcfc798
Johan Lund <johanjohan.lund@gmail.com>
2017-03-27 14:25:07 +0200

    added require and removed include and formatted properly

--- api/connect.php
+++ api/connect.php
@@ -1,20 +1,20 @@
-<?php 
-require_once'session.php';
-require_once'database.php';
+<?php
+require 'session.php';
+require 'database.php';
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-<meta charset="utf-8">
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
 <form action="login.php" method="POST" name="login">
-     <input type="text" name="email" value="" placeholder="E-post" /><br>
-     <input type="password" name="password" value="" placeholder="Lösenord"/><br> 
+    <input type="text" name="email" value="" placeholder="E-post"/><br>
+    <input type="password" name="password" value="" placeholder="Lösenord"/><br>
     <button type="submit" name="login">Logga in</button>
 </form>
 
-<?php 
+<?php
 
 echo error_message(); // kalla på sessionfunktion
 
