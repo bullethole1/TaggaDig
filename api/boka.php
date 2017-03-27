f3991887bbde5ed7082078595a807747c1a28f28
Johan Lund <johanjohan.lund@gmail.com>
2017-03-27 14:13:27 +0200

    removed include once and added require and removed closing php tagg

--- api/boka.php
+++ api/boka.php
@@ -1,11 +1,4 @@
-<!--<form action="logout.php" method="POST">
-
-    <button type="submit" name="">LOGGA UT</button>
-</form> -->
 <?php
-require_once'session.php';
-include_once'database.php';
-require_once'create_order.php';
-
-var_dump( $_SESSION['userid']);
-?>
+require'session.php';
+require 'database.php';
+require'create_order.php';
