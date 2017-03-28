7b70e1a37d7284dbd71733b43436935797eb0c5f
Johan Lund <johanjohan.lund@gmail.com>
2017-03-28 13:45:40 +0200

    binded parameter to pdo and removed closing php tagg and added require instead of include

--- api/user_information.php
+++ api/user_information.php
@@ -1,15 +1,13 @@
 <?php
-require_once'session.php'; 
-include_once'database.php';
+require 'session.php';
+require 'database.php';
 failed();
 
 
 //SE ANVÄNDARINFO
-$sql_user = "SELECT business, firstName, lastName, email, phone  FROM `members` WHERE `id` = {$_SESSION['userid']}  ";
+$sql_user = "SELECT business, firstName, lastName, email, phone  FROM `members` WHERE `id` = :sessionId";
 $row = $pdo->prepare($sql_user);
-$row->execute();
-$resultat=$row->fetchAll(PDO::FETCH_ASSOC);
-$main_user = array('data'=> $resultat);
-echo json_encode($main_user);
-
-?> 
+$row->execute(['sessionId' => $_SESSION['userid']]);
+$resultat = $row->fetchAll(PDO::FETCH_ASSOC);
+$main_user = array('data' => $resultat);
+echo json_encode($main_user, JSON_UNSCAPE_UNICODE);
