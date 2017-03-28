160b18178eb49bc3a8705f865780dcb041380119
Johan Lund <johanjohan.lund@gmail.com>
2017-03-28 12:03:01 +0200

    correct formatting and removing of closing php tagg

--- api/delete_members.php
+++ api/delete_members.php
@@ -1,13 +1,9 @@
 <?php
-require_once 'session.php';
-include_once 'database.php';
-// hämta alla ordrar
+require 'session.php';
+require 'database.php';
 
-if(isset($_POST['delete'])){
+if (isset($_POST['delete'])) {
     $sql = "DELETE FROM `members`  WHERE `members` . `id` = :id_number";
-$stm_delete = $pdo->prepare($sql);
-$stm_delete->execute(array ('id_number' => ($_POST['delete'] ) ));
-// echo "tagit bort";
+    $stm_delete = $pdo->prepare($sql);
+    $stm_delete->execute(array('id_number' => ($_POST['delete'])));
 }
-
-?>
