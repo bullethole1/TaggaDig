17371edcb198aa0518b2a8013198d5509b42d31e
Johan Lund <johanjohan.lund@gmail.com>
2017-03-28 12:47:20 +0200

    correct formatting and removing of closing php tagg and added require instead of include

--- api/show_orders_admin.php
+++ api/show_orders_admin.php
@@ -1,14 +1,12 @@
 <?php
-require_once 'session.php';
-include_once 'database.php';
-// hämta alla ordrar
-// admin();
+require 'session.php';
+require 'database.php';
+if ($_SESSION['user_type'] == 0) {
 
-$sql_order = "SELECT *  FROM `orders` WHERE `orderNr` > 0 ";
-$row = $pdo->prepare($sql_order);
-$row->execute();
-$resultat=$row->fetchAll(PDO::FETCH_ASSOC);
-$main_order = array('data'=> $resultat);
-echo json_encode($main_order);
-
-?>
+    $sql_order = "SELECT *  FROM `orders` WHERE `orderNr` > 0 ";
+    $row = $pdo->prepare($sql_order);
+    $row->execute();
+    $resultat = $row->fetchAll(PDO::FETCH_ASSOC);
+    $main_order = array('data' => $resultat);
+    echo json_encode($main_order, JSON_UNESCAPED_UNICODE);
+}
