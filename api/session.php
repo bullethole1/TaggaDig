b753af65058ffd2b10ac324ea7787c15864948d5
Johan Lund <johanjohan.lund@gmail.com>
2017-03-28 13:57:00 +0200

    renamed function and admin functions for clarification and added require instead of include

--- api/session.php
+++ api/session.php
@@ -1,38 +1,34 @@
 <?php
 session_start();
-include_once 'database.php';
+require 'database.php';
 
 // om fel inlogg, skriv ut errormedd. 
-function error_message(){
+function error_message()
+{
 
-if(isset($_SESSION['message'])  ){
- $output = ($_SESSION['message']); 
-$_SESSION['message'] = null;
-return $output;
+    if (isset($_SESSION['message'])) {
+        $output = ($_SESSION['message']);
+        $_SESSION['message'] = null;
+        return $output;
+    }
+
 }
 
-}
 
-
-// OM man inte är inloggad skicka tillbaka till connect.php
-function  failed(){
-    if(!isset ($_SESSION['logged_in']) ){
-        header("Location: connect.php");
+// OM man inte är inloggad skicka tillbaka till start_page.php
+function user_login_failed()
+{
+    if (!isset ($_SESSION['logged_in'])) {
+        header("Location: start_page.php");
         exit;
     }
 }
 
 
-function admin(){
-    if(!isset($_SESSION['usertype'])){
-        header("Location: connect.php");
+function admin_login_failed()
+{
+    if (!isset($_SESSION['usertype'])) {
+        header("Location: start_page.php");
         exit;
     }
 }
-
-
-
-// function json($data) {
-//     header('Content-Type: application/json');
-//     echo json_encode($data);
-// }
